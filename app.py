0dac80bdf3f1e7ecc07c0c5814d30c5da7aaef5d
mabus <m.buscaroli@outlook.com>
2022-07-06 09:53:47 +0100

    setup database

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+# select a database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+# initialise the database
+db = SQLAlchemy(app)
+
+
+class Card(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    question = db.Column(db.String(500), nullable=False)
+    answer = db.Column(db.String(500), nullable=False)
+    created_at = db.Column(db.DateTime, defaulr=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<Card {self.id}"
 
 
 @app.route('/')
